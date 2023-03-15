5fdacc14a72d9b21efcfa4f55170e530a4532e11
Md Abdul Matin <matinpcc@yahoo.com>
2023-03-15 22:37:30 +0100

    Search box created

---
+++ Jobchasers/src/Components/Search.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function Search() {
+  const [value, setValue] = useState("");
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+  const onSearch = (searchTerm) => {
+    console.log("search", searchTerm);
+  };
+  return (
+    <div className="search">
+      <h1>Search</h1>
+      <div className="search-box">
+        <div className="search-inner">
+          <input type="text" value={value} onChange={onChange} />
+          <button onClick={() => onSearch(value)}> Search</button>
+        </div>
+      </div>
+    </div>
+  );
+}
